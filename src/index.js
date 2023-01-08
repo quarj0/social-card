42890300f62bbad5773e222cffdee25c79bc11a3
Owusu Ansah Kwadwo <54241472+hacks-and-codes@users.noreply.github.com>
2023-01-08 12:34:54 +0000

    update in react files

--- src/index.js
+++ src/index.js
@@ -4,13 +4,9 @@ import SocialIcons   from './SocialIcons';
 import './index.css';
 import Body from './components/body';
 import Navbar from './components/navbar';
-import Project from './components/project'
-import Service from './components/services'
-import Contact from './components/contact'
+
 import {
   BrowserRouter as Router,
-  Routes,
-  Route,
 } from "react-router-dom";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -21,11 +17,6 @@ root.render(
     <Navbar>
       {Navbar}
     </Navbar>
-    <Routes>
-      <Route path="/" element={<Project />} />
-      <Route path="/" element={<Service />} />
-      <Route path="/" element={<Contact />} />
-    </Routes>
     </nav>
     </Router>
     </div>
